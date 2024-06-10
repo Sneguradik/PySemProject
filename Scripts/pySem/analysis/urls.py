9320baed0c4936f9226ff37bc5e5112d4c86486d
Sneguradik <rrtr845@gmail.com>
2024-06-10 09:59:41 +0300

    Charts added

--- Scripts/pySem/analysis/urls.py
+++ Scripts/pySem/analysis/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, add_data, get_report, get_dataset_view, get_chart, create_dataset, insert_dataset
+from .views import index, add_data, get_report, get_dataset_view, get_chart, create_dataset, insert_dataset, get_chart_data
 
 urlpatterns = [
     path('', index, name='index'),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('dataset/<int:dataset_id>/page/<int:page>', get_dataset_view, name='get_dataset'),
     path('dataset/chart/<int:dataset_id>', get_chart, name='get_chart'),
     path('dataset/create', create_dataset, name='create_dataset'),
-    path('dataset/insert', insert_dataset, name='insert_dataset')
+    path('dataset/insert', insert_dataset, name='insert_dataset'),
+    path('dataset/chart/data/<int:dataset_id>', get_chart_data, name='get_chart_data')
 ]
--- Scripts/pySem/analysis/views.py
+++ Scripts/pySem/analysis/views.py
@@ -1,17 +1,21 @@
 import datetime
+import io
 import os
 import numpy as np
 import pandas
 from django.core.paginator import Paginator
-from django.http import HttpResponse
+from django.http import HttpResponse, FileResponse, JsonResponse
 from django.shortcuts import render, redirect
 from .forms import CreateDatasetForm, AddDataForm
 from .models import DataSet, DataSetValue
+import matplotlib.pyplot as plt
+import matplotlib as mpt
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    datasets = DataSet.objects.all()
+    return render(request, 'index.html', {'datasets': datasets})
 
 
 def add_data(request):
@@ -23,8 +27,10 @@ def get_report(request):
 
 
 def get_dataset_view(request, dataset_id, page):
+    per_page = 10
+
     dataset = DataSet.objects.get(id=dataset_id)
-    pg = Paginator(DataSetValue.objects.filter(dataSet=dataset),25)
+    pg = Paginator(DataSetValue.objects.filter(dataSet=dataset), per_page)
 
 
 
@@ -32,13 +38,45 @@ def get_dataset_view(request, dataset_id, page):
         'dataset': dataset,
         'values': pg.page(page),
         'pages': list(range(1,pg.num_pages+1)),
-        'offset': (page-1)*25
+        'offset': (page-1)*per_page
     }
     return render(request, 'dataset_page.html', page_data)
 
 
 def get_chart(request, dataset_id):
-    pass
+    dataset = DataSet.objects.get(id=dataset_id)
+    vals = DataSetValue.objects.filter(dataSet=dataset).order_by('timestamp')
+
+    plt.figure(figsize=(10, 5))
+    dates = [val.timestamp for val in vals]
+    values = [val.value for val in vals]
+    plt.plot(dates, values, marker='o')
+
+    plt.gcf().autofmt_xdate()
+
+    plt.xlabel('Date')
+    plt.ylabel('Value')
+    plt.title('Line Plot of Values over Time')
+
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format='png')
+    buffer.seek(0)
+
+    response = HttpResponse(buffer, content_type='image/png')
+    response['Content-Disposition'] = 'attachment; filename="chart.png"'
+
+    return response
+
+def get_chart_data(request, dataset_id):
+    dataset = DataSet.objects.get(id=dataset_id)
+    vals = DataSetValue.objects.filter(dataSet=dataset).order_by('timestamp')
+
+    resp = {
+        'values': [val.value for val in vals],
+        'labels': [val.timestamp.strftime('%m/%Y') for val in vals]
+    }
+
+    return JsonResponse(resp)
 
 def create_dataset_values(f, dataset):
     print(dataset.id)
